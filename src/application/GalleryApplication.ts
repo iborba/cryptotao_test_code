95e934309fa703539def599a3f9694088cdd184b
Israel Borba <israel.borba@stoneridge.com>
2022-04-22 11:08:01 -0300

    gallery application construction

--- src/application/GalleryApplication.ts
+++ src/application/GalleryApplication.ts
@@ -1,1 +1,18 @@
-export class GalleryApplication { }
+export class GalleryApplication {
+  listNFT(body: { galleryId: string; }) {
+    throw new Error('Method not implemented.');
+  }
+  addNFT(body: { galleryId: string; NFTs: { name: string; description: string; image: string; external_url: string; origin: { name: string; symbol: string; description: string; seller_fee_basis_points: number; image: string; external_url: string; attributes: never[]; collection: { ...; }; properties: { ...; }; }; }[]; }) {
+  throw new Error('Method not implemented.');
+}
+deleteNFT(body: { galleryId: string; nftId: string; }) {
+  throw new Error('Method not implemented.');
+}
+deleteGallery(body: { id: string; }) {
+  throw new Error('Method not implemented.');
+}
+createGallery(body: { name: string; owner: string; }) {
+  throw new Error('Method not implemented.');
+} 
+
+}
