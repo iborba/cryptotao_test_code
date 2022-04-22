cd3ca2ac8d978433091ba486c0ef762909ab7989
Israel Borba <israel.borba@stoneridge.com>
2022-04-22 11:30:14 -0300

    gallery scaffold

--- src/application/GalleryApplication.ts
+++ src/application/GalleryApplication.ts
@@ -1,18 +1,24 @@
-export class GalleryApplication {
-  listNFT(body: { galleryId: string; }) {
+export class GalleryApplication implements IGalleryApplication {
+  listNFT(id: string, nftId?: string) {
     throw new Error('Method not implemented.');
   }
-  addNFT(body: { galleryId: string; NFTs: { name: string; description: string; image: string; external_url: string; origin: { name: string; symbol: string; description: string; seller_fee_basis_points: number; image: string; external_url: string; attributes: never[]; collection: { ...; }; properties: { ...; }; }; }[]; }) {
-  throw new Error('Method not implemented.');
+  //   addNFT(id: string; NFTs: { name: string; description: string; image: string; external_url: string; origin: { name: string; symbol: string; description: string; seller_fee_basis_points: number; image: string; external_url: string; attributes: never[]; collection: { ...; }; properties: { ...; }; }; }[]; }) {
+  //   throw new Error('Method not implemented.');
+  // }
+  deleteNFT(id: string, nftId: string) {
+    throw new Error('Method not implemented.');
+  }
+  deleteGallery(id: string) {
+    throw new Error('Method not implemented.');
+  }
+  createGallery(name: string, ownerId: string) {
+    throw new Error('Method not implemented.');
+  }
 }
-deleteNFT(body: { galleryId: string; nftId: string; }) {
-  throw new Error('Method not implemented.');
-}
-deleteGallery(body: { id: string; }) {
-  throw new Error('Method not implemented.');
-}
-createGallery(body: { name: string; owner: string; }) {
-  throw new Error('Method not implemented.');
-} 
 
+export interface IGalleryApplication {
+  id: string;
+  nftId: string;
+  ownerId: string;
+  name: string;
 }
--- src/application/__tests__/GalleryApplication.ts
+++ src/application/__tests__/GalleryApplication.ts
@@ -2,43 +2,55 @@ import { GalleryApplication } from '../GalleryApplication';
 
 describe('GalleryApplication', () => {
   it('should create a personal gallery if owner exists', async () => {
-    const body = {
-      name: 'test gallery',
-      owner: '6e9b27dc-7e9d-48dd-a64b-c49f014b9fb9',
-    };
+    const name = 'test gallery';
+    const ownerId = '6e9b27dc-7e9d-48dd-a64b-c49f014b9fb9';
 
     const gallery = new GalleryApplication();
-    const result = await gallery.createGallery(body);
+    const result = await gallery.createGallery(name, ownerId);
 
     expect(result.message).toEqual('Gallery succesfully created.');
   });
 
   it('should not allow to delete a gallery if it contains elements inside', async () => {
-    const body = {
-      id: '7828e6cc-fc0e-4477-9625-02fedca814d8',
-    };
-
+    const id = '7828e6cc-fc0e-4477-9625-02fedca814d8';
     const gallery = new GalleryApplication();
-    const result = await gallery.deleteGallery(body);
+    const result = await gallery.deleteGallery(id);
 
     expect(result.message).toEqual("This gallery can't be deleted because is non-empty");
   });
 
   it('should delete one or more NFTs from the gallery', async () => {
-    const body = {
-      galleryId: '7828e6cc-fc0e-4477-9625-02fedca814d8',
-      nftId: '55aee5fb-00d6-48f0-897e-eeec36e050b9',
-    };
+    const id = '7828e6cc-fc0e-4477-9625-02fedca814d8';
+    const nftId = '55aee5fb-00d6-48f0-897e-eeec36e050b9';
 
     const gallery = new GalleryApplication();
-    const result = await gallery.deleteNFT(body);
+    const result = await gallery.deleteNFT(id, nftId);
 
     expect(result.message).toEqual('NFTs successfully removed from gallery.');
   });
 
+  it('should list all NFTs in a gallery', async () => {
+    const id = '7828e6cc-fc0e-4477-9625-02fedca814d8';
+    const gallery = new GalleryApplication();
+    const result = await gallery.listNFT(id);
+
+    expect(Array.isArray(result)).toBe(true);
+    expect(result).toBeGreaterThan(0);
+  });
+
+  it('should list one NFT in a gallery', async () => {
+    const id = '7828e6cc-fc0e-4477-9625-02fedca814d8';
+    const nftId = '55aee5fb-00d6-48f0-897e-eeec36e050b9';
+
+    const gallery = new GalleryApplication();
+    const result = await gallery.listNFT(id, nftId);
+
+    expect(result).toEqual(1);
+  });
+
   it('should add a NFT to a gallery', async () => {
     const body = {
-      galleryId: '7828e6cc-fc0e-4477-9625-02fedca814d8',
+      id: '7828e6cc-fc0e-4477-9625-02fedca814d8',
       NFTs: [
         {
           name: 'SOL Parasite #4679',
@@ -82,33 +94,9 @@ describe('GalleryApplication', () => {
     expect(result.message).toEqual('NFTs successfully inserted to gallery.');
   });
 
-  it('should list all NFTs in a gallery', async () => {
-    const body = {
-      galleryId: '7828e6cc-fc0e-4477-9625-02fedca814d8',
-    };
-
-    const gallery = new GalleryApplication();
-    const result = await gallery.listNFT(body);
-
-    expect(Array.isArray(result)).toBe(true);
-    expect(result).toBeGreaterThan(0);
-  });
-
-  it('should list one NFT in a gallery', async () => {
-    const body = {
-      galleryId: '7828e6cc-fc0e-4477-9625-02fedca814d8',
-      nftId: '55aee5fb-00d6-48f0-897e-eeec36e050b9',
-    };
-
-    const gallery = new GalleryApplication();
-    const result = await gallery.listNFT(body);
-
-    expect(result).toEqual(1);
-  });
-
   it('should not duplicate elements inside the gallery and return NFT XXX already in the gallery', async () => {
     const body = {
-      galleryId: '7828e6cc-fc0e-4477-9625-02fedca814d8',
+      id: '7828e6cc-fc0e-4477-9625-02fedca814d8',
       NFTs: [
         {
           name: 'SOL Parasite #4679',
